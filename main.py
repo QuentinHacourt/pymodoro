ce49908b7a1656ee510ad5e791a4f76e2c1efbd2
QuentinHacourt <quentin.hacourt@gmail.com>
2022-05-30 17:10:32 +0200

    refactored OOP

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import curses
 from curses import wrapper
 
-def writeWord(y, x, word, stdscr):
-    stdscr.addstr(y, x, word[0], curses.A_UNDERLINE)
-    stdscr.addstr(y, x+1, word[1:])
+class Menu:
+    def __init__(self, stdscr, title, items):
+        self.stdscr = stdscr
+        self.title = title
+        self.items = items
+
+    def writeWord(self, y, x, word):
+        self.stdscr.addstr(y, x, word[0], curses.A_UNDERLINE)
+        self.stdscr.addstr(y, x+1, word[1:])
+
+    def makeMenu(self):
+        self.stdscr.clear()
+
+        self.stdscr.addstr(3, 10, self.title)
+
+        i = 5
+        for item in self.items:
+            self.writeWord(i, 10, item)
+            i +=1
+
+        self.stdscr.refresh()
+        self.stdscr.getkey()
 
 def main(stdscr):
-    stdscr.clear()
+    title = 'What do you want to do?'
+    items = [ 'Pomodoro', 'Short Break', 'Long Break', 'Quit' ]
+    mainMenu = Menu(stdscr, title, items)
+    mainMenu.makeMenu()
 
-    stdscr.addstr(3, 10, mainMenuTitle)
-
-    i = 5
-    for menuItem in mainMenuItems:
-        writeWord(i, 10, menuItem, stdscr)
-        i +=1
-
-    stdscr.refresh()
-    stdscr.getkey()
-
-
-mainMenuTitle = 'What do you want to do?'
-mainMenuItems = [ 'Pomodoro', 'Short Break', 'Long Break', 'Quit' ]
 wrapper(main)
